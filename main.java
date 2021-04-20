3d98f9f7f58efd9c862f39a3483848a80e54be87
secimpalut <secimpalut28@gmail.com>
2021-04-21 01:24:38 +0300

    v01

--- main.java
+++ main.java
@@ -1,3 +1,5 @@
 public static void main(String args[]) {
-    System.out.println("Herkes local da yeni branch olusturup version guncellemesi yapmalı...")
+    System.out.println("Herkes local da yeni branch olusturup version guncellemesi yapmalı...");
+    System.out.println("Secim kisisi ekledi...");
+
 }
