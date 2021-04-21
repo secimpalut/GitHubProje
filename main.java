7f023f089f63003ac310652853cfec19619eb3ec
Fatih Akbulut <58088079+fatihakbuluttr@users.noreply.github.com>
2021-04-21 09:57:22 +0300

    Update main.java

--- main.java
+++ main.java
@@ -1,5 +1,6 @@
 public static void main(String args[]) {
     System.out.println("Herkes local da yeni branch olusturup version guncellemesi yapmalı...");
     System.out.println("Secim kisisi ekledi...");
-
+    System.out.println("Fatih geldi ve ekledi.");
+    System.out.println("Secim abi bir de branch oluştursan hayrına da masteri bozmasak:)");
 }
